d7501bc673ee84bb1ae1119d23f8cff2b558718a
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-07 17:27:26 +0530

    Add files via upload

---
+++ positive_or_negative.java
@@ -0,0 +1,24 @@
+//positive or nigitive
+
+import java.lang.*;
+class positive_or_negative
+{
+public static void main(String args[])
+{
+int num=-5;
+
+if(num==0)
+{
+System.out.println("Number is Zero");
+}
+else if(num > 0)
+{
+System.out.println("number is positive");
+}
+else
+{
+System.out.println("Number is negative");
+}
+}
+
+}
