6d98225a00d47a29345b41d31625e3519f75eef4
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-07 17:34:33 +0530

    Add files via upload

---
+++ Do_while_loop.java
@@ -0,0 +1,18 @@
+//Do while loop.
+
+import java.lang.*;
+
+class Do_while_loop
+{
+public static void main(String args[])
+{
+int x=1;
+
+do
+{
+System.out.println(x);
+x++;
+}
+while(x<=10);
+}
+}
