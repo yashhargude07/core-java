eeac769ab834adfb157990d62d1e8f424dc31422
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-07 17:43:19 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,17 @@
+//while loop print one to ten number.
+
+import java.lang.*;
+
+class while_loop
+{
+public static void main(String args[])
+{
+int x=1;
+while(x<=10)
+{
+System.out.println(x);
+x++;
+}
+}
+
+}
