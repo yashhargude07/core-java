8284015c70e023d6e18ad54447f43520a240121d
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-19 11:26:34 +0530

    Add files via upload

---
+++ relational_operator.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+class relational_operator
+{
+public static void main(String args[])
+
+{
+int x = 10;
+int y = 20;
+
+if(x == y)
+System.out.println("x == y");
+
+if(x != y)
+System.out.println("x != y");
+
+if(x > y)
+System.out.println("x > y");
+
+if(x < y)
+System.out.println("x < y");
+
+if(x <= y)
+System.out.println("x <= y");
+
+}
+}
