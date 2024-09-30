455f4d63e761e6cc160ff5484fe01ec271de8012
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-09-30 02:48:02 -0700

    Add files via upload

---
+++ TreeSetDemo.java
@@ -0,0 +1,16 @@
+import java .util.*;
+public class TreeSetDemo
+{
+public static void main(String args[])
+{
+TreeSet ts = new TreeSet();
+ts.add("Z");
+ts.add("C");
+ts.add("A");
+ts.add("B");
+ts.add("E");
+ts.add("F");
+ts.add("D");
+System.out.println(ts);
+}
+}
