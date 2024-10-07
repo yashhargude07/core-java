53f54b8138936556f557113b82358ec06a848e73
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-10-07 15:21:48 +0530

    Add files via upload

---
+++ CopyFile.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class CopyFile
+
+{
+public static void main(String args[])throws IOException
+{
+int ch;
+FileInputStream fin = new FileInputStream(args[0]);
+FileOutputStream fout = new FileOutputStream(args[1]);
+while((ch=fin.read())!=-1);
+fout.write(ch);
+fin.close();
+System.out.println("1 file Copied");
+}
+}
